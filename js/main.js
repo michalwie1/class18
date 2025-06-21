bef8ff231df51189ec341db44cfba9adfa7fe02e
michalwie1 <michaliwieder@gmail.com>
2025-06-21 18:12:22 +0300

    add .ball size dynamic change onclick

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+var gSize = 150
+
 function onBallClick(elBall){
-    elBall.style.width = '50px'
-    elBall.style.height = '50px'
+    if(elBall.style.width === '400px') {
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+    }
+    else{
+        console.log(elBall.style)
+            elBall.style.width =  `${gSize}px`
+            elBall.style.height =  `${gSize}px`
+            gSize += 50
+    }
+    
+
 }
